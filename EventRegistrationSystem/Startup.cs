8abf6d7389d81d53806a6e42936624157bc6d33b
Hill-ai <ahill0991@gmail.com>
2018-09-25 20:33:46 -0400

    Add project files.

---
+++ EventRegistrationSystem/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EventRegistrationSystem.Startup))]
+namespace EventRegistrationSystem
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
