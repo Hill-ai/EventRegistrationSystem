4b54a16bdb859a67043e3ca9519d4c5650a02c31
Daniel <JDanielO@users.noreply.github.com>
2018-10-25 10:55:39 -0400

    Integrate Ninject in the NinjectWebCommon.cs File

--- EventRegistrationSystem/App_Start/NinjectWebCommon.cs
+++ EventRegistrationSystem/App_Start/NinjectWebCommon.cs
@@ -53,6 +53,8 @@ namespace EventRegistrationSystem.App_Start
         /// <param name="kernel">The kernel.</param>
         private static void RegisterServices(IKernel kernel)
         {
+            System.Web.Mvc.DependencyResolver.SetResolver(new
+            EventRegistrationSystem.Infrastructure.NinjectDependencyResolver(kernel));
         }        
     }
 }
