172bc6360963dda116fa26ec058a79fd23a6aa85
Daniel <JDanielO@users.noreply.github.com>
2018-10-25 11:11:23 -0400

    add company model

---
+++ EventRegistrationSystem/Models/Company.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EventRegistrationSystem.Models
+{
+    public class Company
+    {
+        public int CompanyID { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string PrimaryContactName { get; set; }
+        public string PrimaryContactPhoneNumber { get; set; }
+        public string PrimaryContactEmail { get; set; }
+        public string CompanyWebLink { get; set; }
+
+    }
+}
