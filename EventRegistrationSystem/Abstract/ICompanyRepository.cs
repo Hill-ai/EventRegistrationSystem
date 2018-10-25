9be439ceff5573dba6a1c3b83d1fcd93dcb6f965
Daniel <JDanielO@users.noreply.github.com>
2018-10-25 11:36:50 -0400

    Create Abstract Company Repository
    Uses IEnumerable<T> to allow a caller to obtain a sequence of Company objects

---
+++ EventRegistrationSystem/Abstract/ICompanyRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using EventRegistrationSystem.Models; // I'm not using an Entity folder within a Domain Project to store the models
+
+
+
+namespace EventRegistrationSystem.Abstract
+{
+    public interface ICompanyRepository
+    {
+        IEnumerable<Company> Companies { get; }
+    }
+}
