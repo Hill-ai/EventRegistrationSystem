f25e07bcdbf6b8f90afda6cab37e5adf90a7114b
Daniel <JDanielO@users.noreply.github.com>
2018-11-08 21:15:47 -0500

    update controller for processing db data

--- EventRegistrationSystem/Controllers/CompanyController.cs
+++ EventRegistrationSystem/Controllers/CompanyController.cs
@@ -20,4 +20,33 @@ namespace EventRegistrationSystem.Controllers
             return View(repository.Companies);
         }
     }
+
+/*  public class CompanyController : Controller
+    {
+        [HttpPost]
+        public ActionResult Create(Company @company)
+        {
+            using (var dbContext = new CompaniesDbContext())
+            {
+                //add to the in memory collection
+                dbContext.Companies.Add(@company);
+
+                //persist these data changes to the db
+                dbContext.SaveChanges();
+
+                //go to List action method to start new process
+                return RedirectToAction("List");
+            }
+        }
+
+        public ActionResult List()
+        {
+            using (var dbContext = new CompaniesDbContext())
+            {
+                var companyList = dbContext.Companies.ToList();
+                return View(companyList);
+            }
+        }
+    }
+    **/
 }
