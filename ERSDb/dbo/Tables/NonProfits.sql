4a926eae4fe953e79a1cdd5cc7f311de2596312b
Hill-ai <ahill0991@gmail.com>
2018-12-18 11:17:44 -0500

    Creation of NonProfit Table

---
+++ ERSDb/dbo/Tables/NonProfits.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[NonProfits]
+(
+	[NonProfitId] INT NOT NULL PRIMARY KEY, 
+    [Name] NVARCHAR(200) NOT NULL, 
+    [Address] NVARCHAR(100) NOT NULL, 
+    [City] NVARCHAR(200) NOT NULL, 
+    [Zip] NVARCHAR(10) NOT NULL, 
+    [Phone] NVARCHAR(50) NOT NULL, 
+    [Email] NVARCHAR(100) NOT NULL, 
+    [PrimaryContactName] NVARCHAR(200) NULL, 
+    [PrimaryContactPhone] NVARCHAR(50) NULL, 
+    [PrimaryContactEmail] NVARCHAR(100) NULL, 
+    [WebLink] NVARCHAR(200) NULL
+)
