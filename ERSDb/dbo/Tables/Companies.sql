885027dda2d4ebefc9e40761b38d4506355bd736
Daniel <JDanielO@users.noreply.github.com>
2018-12-18 12:06:05 -0500

    Created Companies table and seedCompanies script

---
+++ ERSDb/dbo/Tables/Companies.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Companies](
+	[CompanyID] [int] IDENTITY(1,1) PRIMARY KEY NOT NULL,
+	[Name] [nvarchar](100) NOT NULL,
+	[Address] [nvarchar](500) NOT NULL,
+	[PhoneNumber] [nvarchar](50) NOT NULL,
+	[Email] [nvarchar](100) NOT NULL,
+	[PrimaryContactName] [nvarchar](100) NOT NULL,
+	[PrimaryContactPhoneNumber] [nvarchar](100) NULL,
+	[PrimaryContactEmail] [nvarchar](100) NOT NULL,
+	[CompanyWebLink] [nvarchar](100) NULL,
+);
