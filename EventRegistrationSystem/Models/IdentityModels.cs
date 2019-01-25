abf5e22b13b8a4a284ce39925d2bcac13514ef45
Daniel <JDanielO@users.noreply.github.com>
2019-01-25 13:14:34 -0500

    delete missing file references

--- EventRegistrationSystem/Models/IdentityModels.cs
+++ EventRegistrationSystem/Models/IdentityModels.cs
@@ -31,7 +31,6 @@ namespace EventRegistrationSystem.Models
         }
 
         public System.Data.Entity.DbSet<EventRegistrationSystem.Models.VolunteerEvent> VolunteerEvents { get; set; }
-        public System.Data.Entity.DbSet<EventRegistrationSystem.Models.Event> Events { get; set; }
 
         public virtual System.Data.Entity.DbSet<EventRegistrationSystem.Models.Company> Companies { get; set; }
     }
