d58ad910fa0dfdfb63277b77ae7ca654dd04574f
dean-machineUNF <if.deanrice.istrue@gmail.com>
2019-03-09 19:53:28 -0500

    added email confirmation functionality to event registration

--- EventRegistrationSystem/Controllers/VolunteerEventsController.cs
+++ EventRegistrationSystem/Controllers/VolunteerEventsController.cs
@@ -4,10 +4,12 @@ using System.Data;
 using System.Data.Entity;
 using System.Linq;
 using System.Net;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 using EventRegistrationSystem.Models;
 using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
 
 namespace EventRegistrationSystem.Controllers
 {
@@ -15,6 +17,31 @@ namespace EventRegistrationSystem.Controllers
     public class VolunteerEventsController : Controller
     {
         //private ApplicationDbContext db = new ApplicationDbContext();
+        private ApplicationUserManager _userManager;// for sending registration email
+
+        public VolunteerEventsController()
+        {
+
+        }
+
+        public VolunteerEventsController(ApplicationUserManager userManager)
+        {
+            UserManager = userManager;
+        }
+
+        public ApplicationUserManager UserManager
+        {
+            //get { return _userManager ?? HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>(); }
+
+            //private set { _userManager = value;  }
+
+
+            get { return _userManager ?? HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>(); }
+            private set { _userManager = value; }
+        }
+
+     
+
 
         // GET: Events
         public ActionResult Index(int? tableOrderBy)
@@ -76,7 +103,7 @@ namespace EventRegistrationSystem.Controllers
         }
 
         // GET: Events/Details/5
-        public ActionResult Details(int? id)
+        public ActionResult Details(int? id, string name, string nonProfit)
         {
 
             using (var db = new ApplicationDbContext())
@@ -94,12 +121,99 @@ namespace EventRegistrationSystem.Controllers
             }
         }
 
+        //trying to register event and send confirmation email
         // POST: Events/Delete/5
         [HttpPost, ActionName("Details")]
         [ValidateAntiForgeryToken]
-        public ActionResult Details(int id)
+        public async Task<ActionResult> Details(int id)
+        {
+            int eventId = id;
+            int nonProfitId;
+            string eventName;
+            string eventNonProfit;
+            string eventStartTime;
+            string eventDate;
+            string nonProfitAddress;
+            string nonProfitCity;
+            string nonProfitPhone;
+            string nonProfitEmail;
+            string nonProfitContactName;
+            string nonProfitContactPhone;
+            string nonProfitContactEmail;
+            string nonProfitWebLink;
+            var identityUserId = User.Identity.GetUserId();
+            bool registrationIsSuccesful = false;
+
+            UsersVolunteerEvent tempUsersVolunteerEvent = new UsersVolunteerEvent();
+            tempUsersVolunteerEvent.UserId = identityUserId;
+            tempUsersVolunteerEvent.VolunteerEventId = eventId;
+
+            using (var db = new ApplicationDbContext())
+            {
+
+
+                //VolunteerEvent @event = db.VolunteerEvents.Find(id);
+                //db.VolunteerEvents.Remove(@event);
+
+                
+
+                db.UsersVolunteerEvents.Add(@tempUsersVolunteerEvent);
+                db.SaveChanges();
+                registrationIsSuccesful = true;
+                eventName = db.VolunteerEvents.Find(id).EventName;
+                eventNonProfit = db.VolunteerEvents.Find(id).NonProfit;
+                eventDate = db.VolunteerEvents.Find(id).EventStartTime.ToShortDateString();
+                eventStartTime = db.VolunteerEvents.Find(id).EventStartTime.ToShortTimeString();
+
+                nonProfitId = db.VolunteerEvents.Find(id).NonProfitID;
+                nonProfitAddress = db.NonProfits.Find(nonProfitId).Address;
+                nonProfitCity = db.NonProfits.Find(nonProfitId).City;
+                nonProfitPhone = db.NonProfits.Find(nonProfitId).Phone;
+                nonProfitEmail = db.NonProfits.Find(nonProfitId).Email;
+                nonProfitContactName = db.NonProfits.Find(nonProfitId).PrimaryContactName;
+                nonProfitContactEmail = db.NonProfits.Find(nonProfitId).PrimaryContactEmail;
+                nonProfitContactPhone = db.NonProfits.Find(nonProfitId).PrimaryContactPhone;
+                nonProfitWebLink = db.NonProfits.Find(nonProfitId).WebLink;
+
+
+
+            }
+
+            if (registrationIsSuccesful)
+            {
+                await UserManager.SendEmailAsync(identityUserId, "Day of Difference Volunteer Event Confirmation" ,"<html>You have succesfully registered for " + eventName + 
+                    "<br><br>" + eventName + "is hosted by " + eventNonProfit + ", and is scheduled to start at " + eventStartTime + " on " + eventDate +
+                    "<br><br>" + eventNonProfit + " is located at " + nonProfitAddress + ", " + nonProfitCity +
+                    
+                    "<br><br>For questions about this event, please contact " + nonProfitContactName + " via phone at " +  nonProfitContactPhone +
+                    " or via email at " + nonProfitContactEmail + 
+                    "<br><br> To contact " + eventNonProfit + ", please call " + nonProfitPhone +
+                    " or send an email to " + nonProfitEmail +
+                    "<br><br> To find out more about " +  eventNonProfit + ", please visit " + nonProfitWebLink + "</html>");
+            }
+
+            return RedirectToAction("EventRegistrationEmailSent", "VolunteerEvents");
+        }
+
+
+        //
+        // GET: /VolunteerEvents/EventRegistrationEmailSent
+        [AllowAnonymous]
+        public ActionResult EventRegistrationEmailSent()
+        {
+            return View();
+        }
+
+
+        /*
+         * working code for saving event registration to DB (no email confirm)
+        // POST: Events/Delete/5
+        [HttpPost, ActionName("Details")]
+        [ValidateAntiForgeryToken]
+        public ActionResult Details(int id, ApplicationUserManager userManager)
         {
 
+            //code for saving event registration in db
             int eventId = id;
             var identityUserId = User.Identity.GetUserId();
 
@@ -123,7 +237,7 @@ namespace EventRegistrationSystem.Controllers
                 return RedirectToAction("Index");
             }
         }
-
+        */
 
 
         //TODO: protect this method specifically, not the whole controller 
@@ -230,6 +344,7 @@ namespace EventRegistrationSystem.Controllers
                 return RedirectToAction("Index");
             }
         }
+
 
         protected override void Dispose(bool disposing)
         {
